852e21f4d458c97bfa75cd1b7df1219822347695
Gabriel <69007475+Friendly-Banana@users.noreply.github.com>
2025-02-22 22:51:36 +0100

    use early return

--- src/handler.rs
+++ src/handler.rs
@@ -4,7 +4,7 @@ use poise::serenity_prelude::{
     Mentionable, Message, UserId,
 };
 use poise::FrameworkContext;
-use rand::{thread_rng, Rng};
+use rand::random;
 use regex::Regex;
 use songbird::input::File;
 use sqlx::query;
@@ -103,46 +103,44 @@ async fn auto_reply(
         .filter(|r| r.keywords.iter().any(|s| content.contains(s)));
 
     for reply in matches {
-        if reply.chance.is_none()
-            || thread_rng()
-                .sample::<f32, rand::distributions::OpenClosed01>(rand::distributions::OpenClosed01)
-                <= reply.chance.unwrap_or(1.0)
-        {
-            let keyword = reply.keywords.first().unwrap();
-            query!("INSERT INTO auto_replies(user_id, keyword, count) VALUES ($1, $2, 1) ON CONFLICT (keyword, user_id) DO UPDATE SET count = auto_replies.count + 1", new_message.author.id.get() as i64, keyword).execute(&data.database).await?;
-            let stats = query!(
+        if reply.chance.is_some_and(|chance| random::<f32>() > chance) {
+            continue;
+        }
+
+        let keyword = reply.keywords.first().unwrap();
+        query!("INSERT INTO auto_replies(user_id, keyword, count) VALUES ($1, $2, 1) ON CONFLICT (keyword, user_id) DO UPDATE SET count = auto_replies.count + 1", new_message.author.id.get() as i64, keyword).execute(&data.database).await?;
+        let stats = query!(
                         "SELECT SUM(count)::int AS count FROM auto_replies WHERE keyword ILIKE '%' || $1 || '%'",
                         keyword
                     )
-                .fetch_one(&data.database)
-                .await?;
-            let amount_replied = stats.count.unwrap_or_default().to_string();
+            .fetch_one(&data.database)
+            .await?;
+        let amount_replied = stats.count.unwrap_or_default().to_string();
 
-            let user = reply.user.to_user(ctx.http()).await?;
-            let desc = reply
-                .description
-                .replace("{user}", &user.to_string())
-                .replace("{replies}", &amount_replied);
-            let mut m = CreateMessage::new();
-            // embeds can't ping
-            if reply.ping {
-                m = m.content(user.mention().to_string());
-            }
-            let message = m.reference_message(new_message).embed(
-                CreateEmbed::new()
-                    .title(&reply.title)
-                    .description(desc)
-                    .colour(reply.colour)
-                    .author(
-                        CreateEmbedAuthor::new(&user.name)
-                            .icon_url(user.avatar_url().unwrap_or(user.default_avatar_url())),
-                    ),
-            );
-            new_message
-                .channel_id
-                .send_message(&ctx.http, message)
-                .await?;
+        let user = reply.user.to_user(ctx.http()).await?;
+        let desc = reply
+            .description
+            .replace("{user}", &user.to_string())
+            .replace("{replies}", &amount_replied);
+        let mut m = CreateMessage::new();
+        // embeds can't ping
+        if reply.ping {
+            m = m.content(user.mention().to_string());
         }
+        let message = m.reference_message(new_message).embed(
+            CreateEmbed::new()
+                .title(&reply.title)
+                .description(desc)
+                .colour(reply.colour)
+                .author(
+                    CreateEmbedAuthor::new(&user.name)
+                        .icon_url(user.avatar_url().unwrap_or(user.default_avatar_url())),
+                ),
+        );
+        new_message
+            .channel_id
+            .send_message(&ctx.http, message)
+            .await?;
     }
     Ok(())
 }
