9f4324696267b9a47a3debee52bff53168d0dd0d
Gabriel <69007475+Friendly-Banana@users.noreply.github.com>
2025-03-19 17:55:12 +0100

    add manpages to debian slim image

--- shuttle_setup_container.sh
+++ shuttle_setup_container.sh
@@ -1,3 +1,12 @@
-add-apt-repository -y ppa:tomtomtom/yt-dlp
+# allow manpage installation
+sed -i '/path-exclude \/usr\/share\/man/d' /etc/dpkg/dpkg.cfg.d/docker
+sed -i '/path-exclude \/usr\/share\/groff/d' /etc/dpkg/dpkg.cfg.d/docker
+
+# add non-free
+sed -i 's/Components: main/Components: main non-free/' /etc/apt/sources.list.d/debian.sources
 apt update
-apt install -y libopus-dev yt-dlp man man-db manpages-posix manpages-dev manpages-posix-dev
+apt install -y libopus-dev man manpages-dev manpages-posix manpages-posix-dev
+apt install --reinstall coreutils
+
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
