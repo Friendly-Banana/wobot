eaed627650da72f7744937fa744126863a2af9bd
Gabriel <69007475+Friendly-Banana@users.noreply.github.com>
2026-02-27 14:57:41 +0100

    better inactive command

--- src/commands/owner/inactive.rs
+++ src/commands/owner/inactive.rs
@@ -1,22 +1,30 @@
-use std::collections::HashSet;
+use std::collections::{HashSet, VecDeque};
 
+use crate::Context;
+use crate::commands::utils;
 use itertools::Itertools;
 use poise::futures_util::StreamExt;
-use poise::serenity_prelude::Mentionable;
+use poise::serenity_prelude::{Mentionable, RoleId};
 use sqlx::query;
 use tracing::warn;
 
-use crate::Context;
-
-/// List inactive users, default 60 days
+/// List inactive users, default 60 days, no bots
 #[poise::command(slash_command, prefix_command, guild_only, owners_only)]
-pub(crate) async fn inactive(ctx: Context<'_>, days: Option<u32>) -> anyhow::Result<()> {
-    ctx.defer_ephemeral().await?;
+pub(crate) async fn inactive(
+    ctx: Context<'_>,
+    days: Option<u32>,
+    include_bots: Option<bool>,
+    exclude_role: Option<RoleId>,
+) -> anyhow::Result<()> {
+    ctx.defer().await?;
+
+    let days = days.unwrap_or(60);
+    let include_bots = include_bots.unwrap_or(false);
     let guild = ctx.guild_id().unwrap();
     let active = query!(
             "SELECT user_id FROM activity WHERE guild_id = $1 AND now() - last_active <= interval '1 day' * $2",
             guild.get() as i64,
-            days.unwrap_or(60) as i32
+            days as i32
         )
         .fetch_all(&ctx.data().database)
         .await?
@@ -30,7 +38,10 @@ pub(crate) async fn inactive(ctx: Context<'_>, days: Option<u32>) -> anyhow::Res
     while let Some(member_result) = members.next().await {
         match member_result {
             Ok(member) => {
-                if !active.contains(&member.user.id.get()) {
+                if !active.contains(&member.user.id.get())
+                    && (!member.user.bot || include_bots)
+                    && exclude_role.is_none_or(|role| !member.roles.contains(&role))
+                {
                     inactive.push(member.user.id);
                 }
             }
@@ -38,10 +49,15 @@ pub(crate) async fn inactive(ctx: Context<'_>, days: Option<u32>) -> anyhow::Res
         }
     }
 
-    ctx.reply(format!(
-        "Inactive: {}",
-        inactive.into_iter().map(|u| u.mention()).join(", ")
-    ))
-    .await?;
+    let mut lines: VecDeque<_> = inactive
+        .into_iter()
+        .map(|u| u.mention().to_string())
+        .chunks(10)
+        .into_iter()
+        .map(|mut chunk| chunk.join(", "))
+        .collect();
+    lines.push_front(format!("Inactive for {} days", days));
+
+    utils::paginate_text(ctx, &mut lines).await?;
     Ok(())
 }
